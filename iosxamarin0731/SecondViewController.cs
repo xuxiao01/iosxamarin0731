81685eeb8b35aea313f74144e94eec315fd35ac9
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-07-31 10:22:08 +0800

    upload

---
+++ iosxamarin0731/SecondViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace iosxamarin0731
+{
+    public partial class SecondViewController : UIViewController
+    {
+        protected SecondViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            Crashes.GenerateTestCrash();
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
